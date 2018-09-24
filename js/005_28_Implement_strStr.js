b6bf75fce5f78f3db00d16220cbef30ca50ef1b3
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-09-24 00:04:35 -0700

    20180922

--- js/005_28_Implement_strStr.js
+++ js/005_28_Implement_strStr.js
@@ -20,3 +20,32 @@ to ask during an interview.
 For the purpose of this problem, we will return 0 when needle is an empty string.
 This is consistent to C's strstr() and Java's indexOf().
 */
+
+// 1.
+var strStr = function(haystack, needle) {
+  for (var i = 0; ; i++) {
+    for (var j = 0; ; j++) {
+      if (j == needle.length) return i;
+      if (i + j == haystack.length) return -1;
+      if (needle.charAt(j) !== haystack.charAt(i + j)) break;
+    }
+  }
+};
+
+// 2. https://leetcode.com/problems/implement-strstr/discuss/163740/Simple-and-Clear-Solution
+var strStr = function(haystack, needle) {
+  var diff = haystack.length - needle.length;
+  if(needle.length==0){
+    return 0;
+  }
+  for(var i = 0; i<=diff;i++){
+    if(haystack.substring(i,needle.length+i)==needle){
+      return i;
+    }
+  }
+  return -1;
+};
+// stringObject.substring(start,stop)  (不包括stop位置)
+// start	必需。一个非负的整数，规定要提取的子串的第一个字符在stringObject中的位置。
+//stop	可选。一个非负的整数，比要提取的子串的最后一个字符在 stringObject 中的位置多1。
+// 如果省略该参数，那么返回的子串会一直到字符串的结尾。
