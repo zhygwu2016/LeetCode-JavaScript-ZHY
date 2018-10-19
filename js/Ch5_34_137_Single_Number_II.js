b58041c97b9226e1f3a91dd91a8df20e676770e9
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-10-18 23:59:45 -0700

    JavaScript

--- js/Ch5_34_137_Single_Number_II.js
+++ js/Ch5_34_137_Single_Number_II.js
@@ -25,7 +25,29 @@ var singleNumber = function(nums) {
 
 };
 
-
+/*
+Solution:
+To solve this problem using only constant space, you have to rethink how the numbers
+are being represented in computers – using bits.
+If you sum the ith bit of all numbers and mod 3, it must be either 0 or 1 due to the
+constraint of this problem where each number must appear either three times or once.
+This will be the ith bit of that "single number".
+A straightforward implementation is to use an array of size 32 to keep track of the total
+count of ith bit.
+*/
 var singleNumber = function(nums) {
-
+  var count = [];
+  for(let i=0; i<32;i++){
+    count.push(0);
+  }
+  var result = 0;
+  for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if ((nums[j] >> i) & 1) {
+        count[i]++;
+      }
+    }
+    result |= ((count[i] % 3) << i);
+  }
+  return result;
 };
